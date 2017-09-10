a0019b1a7a55e29e84350a9cd1f11809247c539e
Sally Buchanan <sabuchanan@comcast.net>
2017-09-09 21:39:12 -0400

    New Project using Selenium to test login to USA Gymnastics website.

--- src/test/java/com/practice/webdriver/MyFirstTest.java
+++ src/test/java/com/practice/webdriver/MyFirstTest.java
@@ -1,6 +1,8 @@
 package com.practice.webdriver;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,13 +19,16 @@ import static java.lang.Thread.sleep;
 *
  */
 public class MyFirstTest {
-    @Test
+
+    private WebDriver driver;
+
+    @Before
     public void startWebDriver() {
 
         //Using the Chrome webdriver, go to USA Gymnastics web page
 
         System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("http://www.usagym.org");
 
 
@@ -31,34 +36,38 @@ public class MyFirstTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement element = driver.findElement(By.className("memberLogin"));
         element.click();
-
-
+    }
+    @Test
+        public void testLogin() {
         /*
         The login Modal opens.
         Find the user name and password fields.  Send bogus id and password.
         Verify the login fails & notifies the user.
          */
-        WebElement loginModal = driver.findElement(By.id("loginPopUp"));
-        WebElement userTxt = loginModal.findElement(By.name("user"));
-        WebElement passTxt = loginModal.findElement(By.name("pass"));
-        userTxt.sendKeys("AnyUserName");
-        passTxt.sendKeys("pass1234");
+            WebElement loginModal = driver.findElement(By.id("loginPopUp"));
+            WebElement userTxt = loginModal.findElement(By.name("user"));
+            WebElement passTxt = loginModal.findElement(By.name("pass"));
+            userTxt.sendKeys("AnyUserName");
+            passTxt.sendKeys("pass1234");
 
-       // Use xpath (from the login window) to find the Login button
-        WebElement lognBtn = loginModal.findElement(By.xpath("//div[@class='ui-dialog-buttonset']/descendant::span[text()='Login']"));
-        lognBtn.click();
+            // Use xpath (from the login window) to find the Login button
+            WebElement lognBtn = loginModal.findElement(By.xpath("//div[@class='ui-dialog-buttonset']/descendant::span[text()='Login']"));
+            lognBtn.click();
 
-        // Hold the window a few seconds so that we can see the Permission Denied message
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            // Hold the window a few seconds so that we can see the Permission Denied message
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Verify the modal is Permission Denied Modal displays and just print its message to sysout.
+            WebElement loginMsg = driver.findElement(By.id("ui-dialog-title-flashContainer"));
+            System.out.println(loginMsg.getText());
         }
 
-        // Verify the modal is Permission Denied Modal displays and just print its message to sysout.
-       WebElement loginMsg = driver.findElement(By.id("ui-dialog-title-flashContainer"));
-       System.out.println( loginMsg.getText());
-
+        @After
+                public void closeUp(){
         // Close current window and any other windows.
         driver.close();
         driver.quit();
